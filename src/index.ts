6b87525b763106db581db4f2e89f831f59c0af38
0912078 <0912078@naver.com>
2023-02-05 15:17:51 +0900

    Feat: failure send 수정

--- src/index.ts
+++ src/index.ts
@@ -116,7 +116,7 @@ function can_login(req, res, next) {
     next(); // 통과 ㄱㄱ 구문
   } else {
     //정보가 없으면 실행
-    res.statjson({ message: "session error" }); // 바꾸기 로그인 페이지 리다이렉션
+    res.status(200).json({ message: "login fail" }); // 바꾸기 로그인 페이지 리다이렉션
   }
 }
 
