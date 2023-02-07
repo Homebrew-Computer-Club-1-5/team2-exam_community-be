3f84969cf47695a3a4e1a33607819c8ae3c15c51
0912078 <0912078@naver.com>
2023-02-07 17:41:45 +0900

    Feat: cicd test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { AppDataSource } from "./data-source";
+import { AppDataSource } from "./data-source"; //cicd test
 const apiRoutes = require("./apis");
 const express = require("express");
 
