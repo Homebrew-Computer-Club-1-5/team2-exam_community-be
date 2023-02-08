85615eb725519b52f5ae7c66a58971b872626d4f
0912078 <0912078@naver.com>
2023-02-08 20:49:24 +0900

    Feat: compareId 사소한 수정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -5,7 +5,7 @@ const postsRoutes = require("./posts.resolver");
 const usersRoutes = require("./users.resolver");
 const commentsRoutes = require("./comments.resolver");
 
-router.use("/apis/users", usersRoutes);
-router.use("/apis/posts", postsRoutes);
-router.use("/apis/comments", commentsRoutes);
+router.use("/users", usersRoutes);
+router.use("/posts", postsRoutes);
+router.use("/comments", commentsRoutes);
 module.exports = router;
